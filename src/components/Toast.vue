332d6f1d26189e4c2357bb362a276ec4ae822d68
Aramdhann <radityamochamad@gmail.com>
2024-03-07 14:51:31 +0700

    create toast component to show notification

---
+++ src/components/Toast.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({
+  action: String,
+});
+
+const message = `There is no content ${props.action}`;
+</script>
+
+<template>
+  <div class="modal">
+    {{ message }}
+    <span class="close">&times;</span>
+  </div>
+</template>
+
+<style scoped>
+.modal {
+  width: fit-content;
+  position: sticky;
+  top: 80px;
+  margin-left: auto;
+  z-index: 100;
+  margin-top: 10px;
+  margin-right: 10px;
+  background: #ae2727;
+  border: 2px solid #cf6f6f;
+  border-radius: 8px;
+  padding: 10px;
+}
+
+.close {
+  margin-left: 10px;
+}
+</style>
