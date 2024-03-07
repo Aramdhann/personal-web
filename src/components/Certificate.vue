ea7ec9e74b7bdac4a5a6ffea974aa14cc61e94cf
Aramdhann <radityamochamad@gmail.com>
2024-03-07 14:49:49 +0700

    call component

---
+++ src/components/Certificate.vue
@@ -0,0 +1,33 @@
+<script setup>
+import CertificateItem from "@/components/CertificateItem.vue";
+import Header from '@/components/Header.vue'
+</script>
+
+<template>
+  <section class="container certificate">
+    <Header>
+      <template #title>
+        Certificate & Competition
+      </template>
+    </Header>
+    <div class="certif-list">
+      <CertificateItem/>
+      <CertificateItem/>
+      <CertificateItem/>
+      <CertificateItem/>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.certif-list {
+  display: flex;
+  padding: 15px 0;
+  gap: 10px;
+  overflow-x: auto;
+}
+
+.certificate {
+  padding: 115px 0px;
+}
+</style>
---
+++ src/components/FeaturedProject.vue
@@ -0,0 +1,29 @@
+<script setup>
+import FeaturedProjectItem from "@/components/FeaturedProjectItem.vue";
+import Header from '@/components/Header.vue'
+</script>
+
+<template>
+  <section class="container">
+    <Header>
+      <template #title>
+        Featured Projects
+      </template>
+    </Header>
+    <div class="project-list">
+      <FeaturedProjectItem/>
+      <FeaturedProjectItem/>
+      <FeaturedProjectItem/>
+      <FeaturedProjectItem/>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.project-list {
+  display: flex;
+  padding: 10px 0;
+  gap: 10px;
+  overflow-x: auto;
+}
+</style>
