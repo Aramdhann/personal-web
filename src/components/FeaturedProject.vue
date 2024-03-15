e7ab06da0e643d6ee7d0ba0a85cd89a405d6e723
Aramdhann <radityamochamad@gmail.com>
2024-03-15 16:22:14 +0700

    styling and call data json for project

--- src/components/FeaturedProject.vue
+++ src/components/FeaturedProject.vue
@@ -1,29 +1,15 @@
 <script setup>
 import FeaturedProjectItem from "@/components/FeaturedProjectItem.vue";
-import Header from '@/components/Header.vue'
+import Header from "@/components/Header.vue";
 </script>
 
 <template>
   <section class="container">
-    <Header>
-      <template #title>
-        Featured Projects
-      </template>
+    <Header :seeAllLink="'/portfolio'" :seeAllText="'See All Project'">
+      <template #title>Featured Projects</template>
     </Header>
     <div class="project-list">
-      <FeaturedProjectItem/>
-      <FeaturedProjectItem/>
-      <FeaturedProjectItem/>
-      <FeaturedProjectItem/>
+      <FeaturedProjectItem />
     </div>
   </section>
 </template>
-
-<style scoped>
-.project-list {
-  display: flex;
-  padding: 10px 0;
-  gap: 10px;
-  overflow-x: auto;
-}
-</style>
--- src/components/FeaturedProjectItem.vue
+++ src/components/FeaturedProjectItem.vue
@@ -1,34 +1,74 @@
 <script setup>
 import Tag from "@/components/Tag.vue";
+import Toast from "@/components/Toast.vue";
+import projects from "@/data/project-data.json";
+import { ref } from "vue";
+
+const showToast = ref(false);
+
+const triggerToast = () => {
+  showToast.value = true;
+  setTimeout(() => {
+    showToast.value = false;
+  }, 3000);
+};
+
+const truncateDescription = (description, limit) => {
+  const words = description.split(" ");
+  if (words.length > limit) {
+    return words.slice(0, limit).join(" ") + " ...";
+  }
+  return description;
+};
 </script>
 
 <template>
-  <div class="card-base-animate">
-    <div class="card card-animate">
-      <div class="card-banner"></div>
-      <div class="card-content">
-        <p class="card-title">title</p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet ipsa
-          sunt deserunt et praesentium nam. Qui, veritatis aut? Adipisci
-          deserunt porro minus delectus ea architecto iure minima sed, esse
-          magni?
-          <a class="text-link">Read more</a>
-        </p>
-        <div class="card-btn-list">
-          <a href="" style="background: var(--main-color)"
-            ><img src="../assets/images/view.png" alt="view project"
-          /></a>
-          <a href="" style="background-color: var(--black)"
-            ><img src="../assets/images/git.png" alt="github project"
-          /></a>
-        </div>
-        <div class="card-tag-list">
-          <Tag>
-            <template #title> tag caption </template>
-          </Tag>
+  <Toast v-if="showToast" />
+  <div
+    v-if="projects && projects.length > 0"
+    v-for="project in projects"
+    :key="project.data.project_id"
+  >
+    <div class="card-base-animate">
+      <div class="card card-animate">
+        <img
+          class="card-banner"
+          :src="`./src/assets/images/projects/${project.data.image}`"
+          :alt="project.data.title"
+        />
+        <div class="card-content">
+          <p class="card-title">{{ project.data.title }}</p>
+          <p class="description">
+            {{ truncateDescription(project.data.description, 20) }}
+          </p>
+          <div class="card-btn-list">
+            <a
+              type="button"
+              :href="project.data.links.view"
+              style="background: var(--main-color)"
+              @click="triggerToast"
+              ><img src="../assets/images/view.png" alt="view project"
+            /></a>
+            <a
+              :href="project.data.links.github"
+              style="background-color: var(--black)"
+              ><img src="../assets/images/git.png" alt="github project"
+            /></a>
+          </div>
+          <div class="card-tag-list">
+            <Tag v-for="(tag, index) in project.data.tags.type" :key="index">
+              <template #title>{{ tag }}</template>
+            </Tag>
+          </div>
         </div>
       </div>
     </div>
   </div>
+  <div v-else>No project data available</div>
 </template>
+
+<style scoped>
+.description {
+  height: 70px;
+}
+</style>
