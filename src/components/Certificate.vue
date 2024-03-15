f3cc9f2a8511c4db0c4e6cf345c54c8af0ddd6da
Aramdhann <radityamochamad@gmail.com>
2024-03-15 16:22:43 +0700

    add certificate compone

--- src/components/Certificate.vue
+++ src/components/Certificate.vue
@@ -1,20 +1,21 @@
 <script setup>
 import CertificateItem from "@/components/CertificateItem.vue";
-import Header from '@/components/Header.vue'
+import Header from "@/components/Header.vue";
 </script>
 
 <template>
   <section class="container certificate">
-    <Header>
-      <template #title>
-        Certificate & Competition
-      </template>
+    <Header
+      :see-all-link="'/certificate'"
+      :see-all-text="'See All Certificate'"
+    >
+      <template #title> Certificate & Competition </template>
     </Header>
     <div class="certif-list">
-      <CertificateItem/>
-      <CertificateItem/>
-      <CertificateItem/>
-      <CertificateItem/>
+      <CertificateItem />
+      <CertificateItem />
+      <CertificateItem />
+      <CertificateItem />
     </div>
   </section>
 </template>
