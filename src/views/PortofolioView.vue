d762ee4a6fdb56fbda983336d5a29db03f9b4fe8
Aramdhann <radityamochamad@gmail.com>
2024-03-20 00:00:11 +0700

    add new page portofolio

--- src/views/PortofolioView.vue
+++ src/views/PortofolioView.vue
@@ -1,1 +1,20 @@
-<template>portofolio</template>
+<script setup>
+import FeaturedProjectItem from '@/components/FeaturedProjectItem.vue'
+</script>
+
+<template>
+  <div class="container project-list">
+    <FeaturedProjectItem/>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+
+.project-list {
+  flex-wrap: wrap;
+}
+</style>
