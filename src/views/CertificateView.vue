5d75ce43f6d2a37542606b59e70b3cc93466295d
Aramdhann <radityamochamad@gmail.com>
2024-03-21 01:41:55 +0700

    add certif view page

--- src/views/CertificateView.vue
+++ src/views/CertificateView.vue
@@ -1,3 +1,22 @@
+<script setup>
+import CertificateItem from '@/components/CertificateItem.vue'
+import certif from "@/data/certif-data.json";
+</script>
+
 <template>
-    certificate
+  <div class="container project-list">
+    <CertificateItem :items="certif"/>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+
+.project-list {
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
